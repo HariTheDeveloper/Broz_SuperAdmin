1ca190c686bac5c4104b584927d7846a6cc40f91
Parthipan19 <parthiban@fuzionest.com>
2021-06-16 13:13:05 +0530

    search employee

--- lib/Push/push.dart
+++ lib/Push/push.dart
@@ -1,3 +1,4 @@
+import 'package:broz_admin/Utitlity/safe_area_container.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
@@ -29,85 +30,87 @@ class _ShareTokenPageState extends State<ShareTokenPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "Device Token",
-          style: TextStyle(color: Colors.white),
-        ),
-        leading: InkWell(
-          onTap: () {
-            Navigator.pop(context);
-          },
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Icon(
-              Icons.arrow_back,
-              color: Colors.white,
-            ),
+    return SafeAreaContainer(
+          child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "Device Token",
+            style: TextStyle(color: Colors.white),
           ),
-        ),
-        backgroundColor: Colors.green,
-      ),
-      body: SizedBox.expand(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            const SizedBox(
-              height: 5.0,
-            ),
-            Align(
-                alignment: Alignment.center,
-                child: Padding(
-                    padding: const EdgeInsets.all(15),
-                    child: Column(
-                      children: <Widget>[
-                        Text(
-                          "Token",
-                          style: TextStyle(color: Colors.grey),
-                        ),
-                        const SizedBox(
-                          height: 10.0,
-                        ),
-                        Text(
-                          "$deviceToken",
-                          style: TextStyle(color: Colors.grey),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(top: 40),
-                          child: Text(
-                            "Please share your device token to admin to get in contact by clicking the share button below !",
-                            style: TextStyle(color: Colors.grey),
-                          ),
-                        ),
-                      ],
-                    ))),
-            const SizedBox(
-              height: 8.0,
-            ),
-            InkWell(
-              onTap: () {
-                final RenderBox box = context.findRenderObject();
-                Share.share(deviceToken,
-                    subject: "Push Notification",
-                    sharePositionOrigin:
-                        box.localToGlobal(Offset.zero) & box.size);
-              },
-              child: Container(
-                color: Colors.green,
-                padding: EdgeInsets.all(10),
-                child: Text(
-                  'Share',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18),
-                  textAlign: TextAlign.center,
-                ),
+          leading: InkWell(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Icon(
+                Icons.arrow_back,
+                color: Colors.white,
               ),
             ),
-          ],
+          ),
+          backgroundColor: Colors.green,
+        ),
+        body: SizedBox.expand(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const SizedBox(
+                height: 5.0,
+              ),
+              Align(
+                  alignment: Alignment.center,
+                  child: Padding(
+                      padding: const EdgeInsets.all(15),
+                      child: Column(
+                        children: <Widget>[
+                          Text(
+                            "Token",
+                            style: TextStyle(color: Colors.grey),
+                          ),
+                          const SizedBox(
+                            height: 10.0,
+                          ),
+                          Text(
+                            "$deviceToken",
+                            style: TextStyle(color: Colors.grey),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 40),
+                            child: Text(
+                              "Please share your device token to admin to get in contact by clicking the share button below !",
+                              style: TextStyle(color: Colors.grey),
+                            ),
+                          ),
+                        ],
+                      ))),
+              const SizedBox(
+                height: 8.0,
+              ),
+              InkWell(
+                onTap: () {
+                  final RenderBox box = context.findRenderObject();
+                  Share.share(deviceToken,
+                      subject: "Push Notification",
+                      sharePositionOrigin:
+                          box.localToGlobal(Offset.zero) & box.size);
+                },
+                child: Container(
+                  color: Colors.green,
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    'Share',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
